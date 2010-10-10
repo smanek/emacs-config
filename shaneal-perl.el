6183aafdfac2d68ef3f48f6b81868a0701560a11
Shaneal Manek <smanek@gmail.com>
2010-10-10 09:19:42 -0500

    cperl mode

---
+++ shaneal-perl.el
@@ -0,0 +1,10 @@
+(provide 'shaneal-perl)
+
+;;prefer cperl-mode to perl-mode
+(mapc
+ (lambda (pair)
+   (if (eq (cdr pair) 'perl-mode)
+       (setcdr pair 'cperl-mode)))
+ (append auto-mode-alist interpreter-mode-alist))
+
+
